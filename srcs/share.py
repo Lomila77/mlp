7de9dbb0e8f936c05b757c48a4394de6d82ee727
gcolomer <gcolomer@protonmail.com>
2025-09-12 16:53:04 +0200

    Add new graph images for analysis
    - Added multiple new PNG files for various metrics including area, compactness, concavity, and texture, among others.
    - Introduced histogram and boxplot visualizations for data analysis.
    - Included confusion matrix heatmap for correlation analysis of numerical variables.

--- srcs/share.py
+++ srcs/share.py
@@ -1,3 +1,44 @@
 
-def plot():
-    pass
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def hist_col(datas: pd.DataFrame):
+    for col in datas.columns:
+        plt.figure(figsize=(12, 8))
+        datas[col].hist()
+        plt.title(col)
+        plt.xlabel(col)
+        plt.ylabel("Value")
+        save_fig(f"hist_{col}")
+
+
+def box_col(datas: pd.DataFrame):
+    for col in datas.columns:
+        plt.figure(figsize=(12, 8))
+        sns.boxplot(x=datas[col])
+        plt.title(col)
+        save_fig(f"box_{col}")
+
+
+def bi_box_col(datas: pd.DataFrame):
+    for col in datas.columns:
+        plt.figure(figsize=(12, 8))
+        sns.boxplot(data=datas, x="diagnosis", y=col)
+        plt.title(f"Boxplot {col} with \"Value\"")
+        plt.xlabel("M, B")
+        plt.ylabel(col)
+        save_fig(f"bix_box_{col}")
+
+
+def confusion_matrix(datas: pd.DataFrame):
+    purge_datas = datas.drop(columns=["id", "diagnosis"])
+    plt.figure(figsize=(20, 15))
+    sns.heatmap(purge_datas.corr(), cmap='coolwarm', annot=True)
+    plt.title('Heatmap de corrélation des variables numériques')
+    save_fig("confusion_matrix")
+
+
+def save_fig(name: str):
+    plt.savefig(f"./analysis/graph/{name}.png")
