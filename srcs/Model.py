f4dde1725742b65672d4e259593c90868dbeea25
gcolomer <gcolomer@protonmail.com>
2025-09-12 15:05:40 +0200

    Add numpy dependency and refactor Model class for improved functionality
    - Introduced requirements.txt to manage dependencies, adding numpy.
    - Refactored Model class to utilize numpy arrays for weights, biases, and activations.
    - Updated methods for softmax, sigmoid, and gradient calculations to support numpy operations.
    - Enhanced backpropagation logic and added gradient descent for weight updates.

--- srcs/Model.py
+++ srcs/Model.py
@@ -1,32 +1,40 @@
 import json
+import numpy as np
 
 
 class Model:
-    weights: list[list[float]]
-    bias: list[list[float]]
-    activations: list[list[float]]
-    gradients: list[list[float]]
-    output: float
+    input_layer: np.ndarray
+    output_layer: np.ndarray
 
-    def __init__(self, weigths: list[list[float]], bias: list[list[float]]) -> None:
-        self.weights = weigths
-        self.bias = bias
+    def __init__(self, shape: list[int]) -> None:
+        self.weights: np.ndarray = np.random.rand(shape)
+        self.bias: np.ndarray = np.random.rand(shape)
+        self.activations: np.ndarray = np.zeros(shape)
+        self.gradients: np.ndarray = np.zeros(shape)
+        self.nias_gradient: np.ndarray = np.zeros(shape)
+        self.output_layer: np.ndarray = np.zeros(2)
+        self.input_layer: np.ndarray = np.zeros(0)  # TODO: Apres analyse des donnees choisir l'input
+        self.learning_rate: float = 0
 
     def get_layer(self, indice: int) -> tuple[list[float], list[float]]:
         return self.weights[indice], self.bias[indice]
 
     def softmax(self):
         """Output activation function"""
-        pass
+        return np.exp(self.output_layer) / np.sum(
+            np.exp(self.output_layer), axis=0)
 
-    def sigmoid(self, z: float):
+    def sigmoid(self, z: np.ndarray):
         """Compression function"""
-        pass
+        z_normalized: np.ndarray = np.zeros_like(z)
+        for idx in range(len(z)):
+            z_normalized[idx] = 1 / 1 + np.exp(-z[idx])
+        return z_normalized
 
-    def partial_derivative_sigmoid(self, z: float):
+    def partial_derivative_sigmoid(self, z: np.ndarray):
         """Compute and return the partial derivative of z sigmoid"""
-        sigmoid: float = self.sigmoid(z)
-        return sigmoid * (1 - sigmoid)
+        sigmoid: np.ndarray = self.sigmoid(z)
+        return np.array([sig * (1 - sig) for sig in sigmoid])
 
     def compute_activation(self):
         """Compute activation for the input given"""
@@ -40,32 +48,32 @@ class Model:
                 new_activations.append(self.sigmoid(z))
             self.activations[idx] = new_activations
 
-    def cross_entropy(self) -> float:
+    def cross_entropy(self, ground_truth: np.ndarray) -> float:
         """Loss function"""
-        pass
+        return -np.sum(ground_truth * np.log(self.softmax()))
 
-    def gradient(self) -> float:
-        pass
+    def gradient_descent(self) -> None:
+        """Weights update after backpropagation"""
+        for idx in range(len(self.weights)):
+            self.weights[idx] -= self.learning_rate * self.gradients[idx]
+            self.bias[idx] -= self.gradients[idx]
 
-    def transpose(self, matrix: list[list[float]]) -> list[list[float]]:
-        pass
+    # TODO: prendre en compte le softmax de fin -> plutot fonction de cout
 
-    # TODO: prendre en compte le softmax de fin
-    def backpropagation(self, idx: int) -> float:
+    def backpropagation(self) -> None:
         """
         Recursive Function which travels along layers,
         from the end to the beginning.
         Set self.gradients
         """
-        upper_gradient: float = sum(self.gradients[idx + 1])
-        curr_gradient: list[float] = []
-        for idx in range(len(self.gradients) - 1, -1, -1):
-            propagated_error: float = sum(self.transpose(self.weights[idx + 1])) * upper_gradient
-            for weight, bias in zip(self.weights[idx], self.bias[idx]):
-
-            
-            self.gradients[idx]
-        pass
+        for idx in range(len(self.gradients) - 2, -1, -1):
+            w: np.ndarray = self.weights[idx]
+            propagated_error: float = w.T @ self.gradients[idx+1]
+            z: np.ndarray = self.weights[idx] @ self.activations[idx] + \
+                self.bias[idx]
+            self.gradients[idx] = propagated_error * \
+                self.partial_derivative_sigmoid(z)
+        self.gradient_descent()
 
     def train_step(self):
         pass
