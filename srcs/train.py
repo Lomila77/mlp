857438e75c4fdc635ad7471b44e87a94139307af
gcolomer <gcolomer@protonmail.com>
2025-10-28 13:39:35 +0100

    Implement training loop with dynamic epochs and loss visualization
    - Added a training loop to the `train` function with a specified number of epochs.
    - Introduced a learning rate parameter in the model initialization.
    - Calculated and plotted training losses after each epoch for better monitoring.
    - Enhanced dataset handling by printing the length of the training dataset.

--- srcs/train.py
+++ srcs/train.py
@@ -1,31 +1,44 @@
+from numpy import mean
 from srcs.Model import Model
 from srcs.data import (
     data,
     load_data
 )
-import numpy as np
+from srcs.share import plot_loss
 
 
 def train():
     # try:
+        batch_size = 3
+        epochs = 180
+        learning_rate = 0.7
+
         out_categories: list = ["M", "B"]
         in_features: list = ["area_worst"]
-        shape: list = [len(in_features), 3, 3, len(out_categories)]
-        print(f"SHAPE: {shape}")
-        batch_size = 3
 
         train_df, val_df = data()
         train_dataset: tuple = load_data(
             train_df, batch_size, in_features, out_categories)
         val_dataset: tuple = load_data(
             val_df, batch_size, in_features, out_categories)
-        model = Model(shape, batch_size)
-        for input, label in train_dataset:
-            print(label.shape)
-            print(type(label))
-            model.train(input, label)
-        # model = Model(shape, input_model, label)
-        # print(model)
+        print(f"Len dataset: {len(train_dataset)}")
+
+        shape: list = [len(in_features), 3, 3, len(out_categories)]
+        model = Model(shape, batch_size, learning_rate)
+        losses = []
+        for epoch in range(epochs):
+            loss = []
+            print("====================================")
+            print("TRAIN:")
+            print(f"Epochs: {epoch}")
+            for input, label in train_dataset:
+                loss.append(model.train(input, label))
+            # for input, label in val_dataset:
+            #     prediction = model.predict(input)
+            losses.append(mean(loss))
+            plot_loss(losses)
+            print("====================================")
+
     # except Exception as e:
     #     print(e)
 
