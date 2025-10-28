0c1b573afb5ffaf14a0769bda9e4390858a41c27
gcolomer <gcolomer@protonmail.com>
2025-10-28 16:27:21 +0100

    Refactor training process to streamline metrics calculation and visualization
    - Updated the `train` function to utilize a single method for training and validation, improving clarity and efficiency.
    - Integrated accuracy, precision, and recall metrics into the training output for better performance monitoring.
    - Enhanced loss visualization by plotting both training and validation losses.
    - Improved error handling to ensure robustness during the training process.

--- srcs/train.py
+++ srcs/train.py
@@ -1,4 +1,3 @@
-from numpy import mean
 from srcs.Model import Model
 from srcs.data import (
     data,
@@ -8,14 +7,13 @@ from srcs.share import plot_loss
 
 
 def train():
-    # try:
+    try:
         batch_size = 3
-        epochs = 180
+        epochs = 80
         learning_rate = 0.7
 
         out_categories: list = ["M", "B"]
         in_features: list = ["area_worst"]
-
         train_df, val_df = data()
         train_dataset: tuple = load_data(
             train_df, batch_size, in_features, out_categories)
@@ -25,22 +23,15 @@ def train():
 
         shape: list = [len(in_features), 3, 3, len(out_categories)]
         model = Model(shape, batch_size, learning_rate)
-        losses = []
-        for epoch in range(epochs):
-            loss = []
-            print("====================================")
-            print("TRAIN:")
-            print(f"Epochs: {epoch}")
-            for input, label in train_dataset:
-                loss.append(model.train(input, label))
-            # for input, label in val_dataset:
-            #     prediction = model.predict(input)
-            losses.append(mean(loss))
-            plot_loss(losses)
-            print("====================================")
-
-    # except Exception as e:
-    #     print(e)
+    
+        metrics = model.train(train_dataset, val_dataset, epochs)
+        plot_loss(metrics["loss"])
+        plot_loss(metrics["v_loss"], name="v_losses")
+        print(f"Accuracy: {(metrics['accuracy'] * 100):.2f} %")
+        print(f"Precision: {(metrics['precision'] * 100):.2f} %")
+        print(f"Recall: {(metrics['recall'] * 100):.2f} %")
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
